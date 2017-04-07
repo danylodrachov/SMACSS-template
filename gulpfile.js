bc20a64f15212dacd5cf35a4026954a11dd3d01c
danylodrachov <danylodrachov@gmail.com>
2017-04-07 18:11:06 +0300

    3rd edit

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     bSync = require('browser-sync').create(),
     refresher = bSync.reload,
     rename = require('gulp-rename'),
-    cleanCSS = require('gulp-clean-css');
+    cleanCSS = require('gulp-clean-css'),
+    wait = require('gulp-wait');
 
 var src = {
     html: 'src/*.html',
@@ -15,18 +16,23 @@ var src = {
 },
     watch = {
         html:'src/*.html',
-        style: 'src/**/**/*.scss'
+        allBase: 'src/scss/base/*.scss',
+        allModules: 'src/scss/modules/*.scss',
+        allStates:  'src/scss/states/*.scss',
+        allMain: 'src/scss/*.scss'
 };
 
 gulp.task('html',  function(){
     gulp.src(src.html)
+    .pipe(wait(500))
     .pipe(gulp.dest(build.html))
     .pipe(refresher({stream: true}));
 });
 
 gulp.task('styles', function(){
 	gulp.src(src.style)
-	.pipe(sass())
+    .pipe(wait(500))
+	.pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS({compatibility: 'ie8'})) 
     .pipe(rename({suffix: '.min'}))
 	.pipe(gulp.dest(build.css))
@@ -40,7 +46,10 @@ gulp.task('server', function(){
 		}
 	});
     gulp.watch(watch.html, ['html']);
-    gulp.watch(watch.style, ['styles']);
+    gulp.watch(watch.allBase, ['styles']);
+    gulp.watch(watch.allModules, ['styles']);
+    gulp.watch(watch.allStates, ['styles']);
+    gulp.watch(watch.allMain, ['styles']);
 });
 
 gulp.task('build', ['html', 'styles'])
